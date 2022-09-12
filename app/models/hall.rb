ad3cf03a390616d43ea9b4377b1571fd964752b5
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-12 14:47:15 +0200

    Add validations

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -1,2 +1,6 @@
 class Hall < ApplicationRecord
+    has_many :seances
+    validates_associated :seances
+    validates :name, presence: true, uniqueness: true, length: { maximum: 255, too_long: "%{count} characters is the maximum allowed" }
+    validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/seance.rb
+++ app/models/seance.rb
@@ -1,4 +1,9 @@
 class Seance < ApplicationRecord
   belongs_to :hall
   belongs_to :movie
+  validates :start, presence: true, comparison: { less_than: :end }
+  validates :end, presence: true, comparison { greater_than: :start }
+  validates :price, presence: true, numericality: { only_decimal: true, greater_than: 0}
+  validates :hall_id, presence: true
+  validates :movie_id, presence: true
 end
