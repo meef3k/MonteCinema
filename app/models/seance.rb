e664f4d7191c3121c1ee2fa59214b14ed07b3290
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-13 14:28:53 +0200

    add finishes_at calculation

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -1,8 +1,12 @@
 class Seance < ApplicationRecord
   belongs_to :hall
   belongs_to :movie
+  before_validation :set_finishes_at
   validates :starts_at, :finishes_at, :price, presence: true
-  validates :starts_at, comparison: { less_than: :finishes_at }
   validates :finishes_at, comparison: { greater_than: :starts_at }
   validates :price, numericality: { only_decimal: true, greater_than: 0 }
+  
+  def set_finishes_at
+    self.finishes_at = starts_at + movie.duration.minutes + 60.minutes
+  end
 end
