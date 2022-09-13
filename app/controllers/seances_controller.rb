908c854396c076cf9d8c0a5544b19405db366979
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-13 14:30:55 +0200

    Change seances controller and view

--- app/controllers/seances_controller.rb
+++ app/controllers/seances_controller.rb
@@ -15,7 +15,7 @@ class SeancesController < ApplicationController
 
     respond_to do |format|
       if @seance.save
-        format.html { redirect_to seance_url(@seance), notice: "Seance was successfully created." }
+        format.html { redirect_to seances_url(@seance), notice: "Seance was successfully created." }
         format.json { render :show, status: :created, location: @seance }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -23,4 +23,14 @@ class SeancesController < ApplicationController
       end
     end
   end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_seance
+      @seance = Seance.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def seance_params
+      params.require(:seance).permit(:starts_at, :finishes_at, :price, :hall_id, :movie_id)
+    end
 end
