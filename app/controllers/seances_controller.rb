2bb025b10d3bd6a8dd4bc2a14f429e6594a24807
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-14 16:05:58 +0200

    edit rubocop controller

--- app/controllers/seances_controller.rb
+++ app/controllers/seances_controller.rb
@@ -3,12 +3,14 @@ class SeancesController < ApplicationController
   def index
     @seances = Seance.includes(:movie, :hall).all
   end
+
   # GET /seances/new
   def new
     @seance = Seance.new
     @halls = Hall.all
     @movies = Movie.all
   end
+
   # POST /seances
   def create
     @seance = Seance.new(seance_params)
@@ -16,20 +18,22 @@ class SeancesController < ApplicationController
     @movies = Movie.all
     respond_to do |format|
       if @seance.save
-        format.html { redirect_to seances_url(@seance), notice: "Seance was successfully created." }
+        format.html { redirect_to seances_url(@seance), notice: 'Seance was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_seance
-      @seance = Seance.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def seance_params
-      params.require(:seance).permit(:starts_at, :finishes_at, :price, :hall_id, :movie_id)
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_seance
+    @seance = Seance.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def seance_params
+    params.require(:seance).permit(:starts_at, :finishes_at, :price, :hall_id, :movie_id)
+  end
 end
