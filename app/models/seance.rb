4b4dd761b0c19e7e35e0b90cd4c4961308eb0c32
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-14 16:08:55 +0200

    Rubocop file fix

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -6,22 +6,23 @@ class Seance < ApplicationRecord
   validates :finishes_at, comparison: { greater_than: :starts_at }
   validates :price, numericality: { only_decimal: true, greater_than: 0 }
   validate :used?
-  
+
   def set_finishes_at
     self.finishes_at = starts_at + movie.duration.minutes + 60.minutes
   end
 
   def used?
     return if hall_available?
-      errors.add(:starts_at, "Hall is used for another seance")
+
+    errors.add(:starts_at, 'Hall is used for another seance')
   end
 
   def hall_available?
     Seance
-      .where(hall_id: hall_id)
-      .where(finishes_at: starts_at .. finishes_at)
-      .or(Seance.where(hall_id: hall_id)
-      .where(starts_at: starts_at .. finishes_at))
+      .where(hall_id:)
+      .where(finishes_at: starts_at..finishes_at)
+      .or(Seance.where(hall_id:)
+      .where(starts_at: starts_at..finishes_at))
       .empty?
   end
 end
