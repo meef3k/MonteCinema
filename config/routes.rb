2e2c4b058e24e50d68f0d70b6fde56fa2089f8eb
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-20 21:12:21 +0200

    [fix] Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :movies
-  resources :seances
+  resources :seances do
+    resources :reservations
+  end
   resources :halls
   # Defines the root path route ("/")
   root 'seances#index'
