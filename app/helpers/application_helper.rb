85ec1459849a7deb4685639d718e4d4157dbd731
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-20 12:32:19 +0200

    Change movie views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def formatted_date(date)
+  def formatted_hour(date)
     date.strftime('%H:%M')
+  end
+
+  def formatted_date(date)
+    date.strftime('%H:%M - %e %B')
   end
 end
