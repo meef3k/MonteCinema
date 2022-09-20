8c3a2764cc9974268a64504bce041f1c44769678
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-20 20:59:26 +0200

    [feature] Create reservation migrations and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_20_120848) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_185132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_120848) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "status", null: false
+    t.bigint "seance_id", null: false
+    t.bigint "promotion_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["promotion_id"], name: "index_reservations_on_promotion_id"
+    t.index ["seance_id"], name: "index_reservations_on_seance_id"
+  end
+
   create_table "seances", force: :cascade do |t|
     t.datetime "starts_at", null: false
     t.datetime "finishes_at", null: false
@@ -48,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_20_120848) do
     t.index ["movie_id"], name: "index_seances_on_movie_id"
   end
 
+  add_foreign_key "reservations", "promotions"
+  add_foreign_key "reservations", "seances"
   add_foreign_key "seances", "halls"
   add_foreign_key "seances", "movies"
 end
