522801e3b7d8288c9c32f40e9766cf520b5ee8ed
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-21 14:11:01 +0200

    [feature] Create index in view and controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,12 @@
 class ReservationsController < ApplicationController
+  before_action :set_seance
+  def index
+    @reservations = @seance.reservations
+  end
+
+  private
+
+  def set_seance
+    @seance = Seance.find(params[:seance_id])
+  end
 end
