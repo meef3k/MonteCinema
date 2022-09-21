74694dba93b85fd6e9a73407d3c666fadd6609e6
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-21 13:36:09 +0200

    [fix] Change status to enum

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,5 @@ class Reservation < ApplicationRecord
   belongs_to :promotion
   validates :status, :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  enum :status, %i[booked accepted cancelled]
 end
--- db/migrate/20220920185132_create_reservations.rb
+++ db/migrate/20220920185132_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.string :email, null: false
-      t.string :status, null: false
+      t.integer :status, null: false
       t.references :seance, null: false, foreign_key: true
       t.references :promotion, null: false, foreign_key: true
 
