3180ba44b83b9ee13a32cad5b64c237c8f801e9d
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-21 11:39:04 +0200

    [fix] Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,44 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-h1 = Hall.create(name: 'Hall 1', capacity: 20)
-h2 = Hall.create(name: 'Hall 2', capacity: 50)
-h3 = Hall.create(name: 'Hall 3', capacity: 50)
-h4 = Hall.create(name: 'Hall 4', capacity: 50)
-h5 = Hall.create(name: 'Hall 5', capacity: 50)
-h6 = Hall.create(name: 'Hall 6', capacity: 100)
-h7 = Hall.create(name: 'Hall 7', capacity: 100)
-h8 = Hall.create(name: 'Hall 8', capacity: 100)
-h9 = Hall.create(name: 'Hall 9', capacity: 100)
-h10 = Hall.create(name: 'Hall 10', capacity: 200)
+Hall.find_or_create_by(:name => 'Hall 1', :capacity => 20)
+(2..5).each do |i|
+    Hall.find_or_create_by(:name => "Hall #{i}", :capacity => 50)
+end
+(6..9).each do |i|
+    Hall.find_or_create_by(:name => "Hall #{i}", :capacity => 100)
+end
+Hall.find_or_create_by(:name => 'Hall 10', :capacity => 200)
 
-m1 = Movie.create(title: 'Harry Potter', description: 'Wizard movie', duration: 154)
-m2 = Movie.create(title: 'Lord of The Rings', description: 'Hobbit movie', duration: 194)
-m3 = Movie.create(title: 'Fast and Furious', description: 'Car movie', duration: 140)
-m4 = Movie.create(title: 'James Bond', description: 'Agent movie', duration: 181)
-m5 = Movie.create(title: 'Minions', description: 'Minions movie', duration: 98)
-m6 = Movie.create(title: 'Hobbit', description: 'Hobbits movie', duration: 143)
-m7 = Movie.create(title: 'Star Wars', description: 'Wars movie', duration: 156)
-m8 = Movie.create(title: 'The Godfather', description: 'Agent movie', duration: 130)
-m9 = Movie.create(title: 'Pulp Fiction', description: 'Comedy movie', duration: 100)
-m10 = Movie.create(title: 'Avengers: Infinity War', description: 'Marvel movie', duration: 134)
+Movie.find_or_create_by(:title => 'Harry Potter', :description => 'Wizard movie', :duration => 154)
+Movie.find_or_create_by(:title => 'Lord of The Rings', :description => 'Hobbit movie', :duration =>  194)
+Movie.find_or_create_by(:title => 'Fast and Furious', :description => 'Car movie', :duration =>  140)
+Movie.find_or_create_by(:title => 'James Bond', :description => 'Agent movie', :duration =>  181)
+Movie.find_or_create_by(:title => 'Minions', :description => 'Minions movie', :duration =>  98)
+Movie.find_or_create_by(:title => 'Hobbit', :description => 'Hobbits movie', :duration =>  143)
+Movie.find_or_create_by(:title => 'Star Wars', :description => 'Wars movie', :duration =>  156)
+Movie.find_or_create_by(:title => 'The Godfather', :description => 'Agent movie', :duration =>  130)
+Movie.find_or_create_by(:title => 'Pulp Fiction', :description => 'Comedy movie', :duration =>  100)
+Movie.find_or_create_by(:title => 'Avengers: Infinity War', :description => 'Marvel movie', :duration =>  134)
 
-s1 = Seance.create(movie_id: 1, hall_id: 10, starts_at: Time.now + 2.days, price: 21)
-s2 = Seance.create(movie_id: 2, hall_id: 9, starts_at: Time.now + 3.days, price: 19)
-s3 = Seance.create(movie_id: 3, hall_id: 8, starts_at: Time.now + 4.days, price: 17)
-s4 = Seance.create(movie_id: 4, hall_id: 7, starts_at: Time.now + 1.days, price: 20)
-s5 = Seance.create(movie_id: 5, hall_id: 6, starts_at: Time.now + 5.days, price: 16)
-s6 = Seance.create(movie_id: 6, hall_id: 5, starts_at: Time.now + 3.days, price: 18)
-s7 = Seance.create(movie_id: 7, hall_id: 4, starts_at: Time.now + 2.days, price: 22)
-s8 = Seance.create(movie_id: 8, hall_id: 3, starts_at: Time.now + 5.days, price: 14)
-s9 = Seance.create(movie_id: 9, hall_id: 2, starts_at: Time.now + 3.days, price: 12)
-s10 = Seance.create(movie_id: 10, hall_id: 1, starts_at: Time.now + 1.days, price: 10)
+(1..10).each do |i|
+    value = 11-i
+    start_time = Time.now + rand(3.days)
+    t_price = (20..30).to_a.sample
+    Seance.find_or_create_by(:movie_id => i, :hall_id => value, :starts_at => start_time, :price => t_price)
+end
 
-p1 = Promotion.create(amount: 3, value: "10% discount for snack set in snack bar")
-p2 = Promotion.create(amount: 4, value: "15% discount for snack set in snack bar")
-p3 = Promotion.create(amount: 5, value: "20% discount for snack set in snack bar")
-p4 = Promotion.create(amount: 6, value: "30% discount for snack set in snack bar")
-p5 = Promotion.create(amount: 7, value: "35% discount for snack set in snack bar")
-p6 = Promotion.create(amount: 8, value: "40% discount for snack set in snack bar")
-p7 = Promotion.create(amount: 9, value: "45% discount for snack set in snack bar")
-p8 = Promotion.create(amount: 10, value: "50% discount for snack set in snack bar")
+(3..10).each do |i|
+    value = (i-1)*5
+    Promotion.find_or_create_by(:value => i, :information => "#{value}% discount for snack set in snack bar")
+  end
