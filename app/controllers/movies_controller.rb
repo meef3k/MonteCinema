1b0f7eac9035d73fecd25791903f923089860b05
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-28 23:18:36 +0200

    [fix] Change variable name

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :set_movie, only: %i[show edit update destroy]
   before_action :set_seances, only: %i[show edit update destroy]
   def index
-    @movie = Movie.all
+    @movies = Movie.all
   end
 
   def show; end
