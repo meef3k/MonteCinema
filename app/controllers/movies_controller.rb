6380720f4b6d90a6071d9353ba2347c520c8ec48
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-09-28 22:52:43 +0200

    [feature] Create CRUD in controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,58 @@
 class MoviesController < ApplicationController
-  def show
+  before_action :set_movie, only: %i[show edit update destroy]
+  before_action :set_seances, only: %i[show edit update destroy]
+  def index
+    @movie = Movie.all
+  end
+
+  def show; end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit; end
+
+  def create
+    @movie = Movie.new(movie_params)
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to movie_url(@movie), notice: 'Movie was successfully created' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @movie.update(movie_params)
+        format.html { redirect_to movie_url(@movie), notice: 'Movie was successfully updated' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @movie.destroy
+
+    respond_to do |format|
+      format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed' }
+    end
+  end
+
+  private
+
+  def set_movie
     @movie = Movie.find(params[:id])
-    @seances = Seance
-               .where(movie_id: params[:id])
-               .this_week
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :duration)
+  end
+
+  def set_seances
+    @seances = Seance.where(movie_id: params[:id]).this_week
   end
 end
