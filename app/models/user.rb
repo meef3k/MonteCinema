2f8364dc089adb7c650ce70a192df0fa27729bfa
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-05 23:06:50 +0200

    [feature] Create password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  PASSWORD_LENGTH = 72
+  enum :role, %i[client cashier manager]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  validate :password_length
+
+  def password_length
+    errors.add(:password, 'Password is too long') if password.nil? || password.bytesize > PASSWORD_LENGTH
+  end
 end
