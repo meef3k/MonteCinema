fea37d05a1df88db0450f66b498f61d811b34209
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-05 23:26:52 +0200

    [test] Create tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "test#{n}@monte.com" }
+    password { 'monte123' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    context 'when password is too long' do
+      it 'validates password length', :aggregate_failures do
+        password = ('m' * 73).to_s
+        user = build(:user, password: password)
+        expect(user).not_to be_valid
+        expect(user.errors).to match_array(['Password Password is too long'])
+      end
+    end
+  end
 end
