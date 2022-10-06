d0a862ba167bd245513eefb95ddf52359a1f6ce4
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-06 15:32:05 +0200

    [feature] Schedule job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,3 +4,8 @@ end
 Sidekiq.configure_client do |config|   
   config.redis = { url: 'redis://127.0.0.1:6379' } 
 end
+
+schedule_file = 'config/schedule.yml'
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
