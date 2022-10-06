269ca5c15470f6ee16b182350d9f1bec271a8cc3
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-06 13:13:06 +0200

    [fix] Change tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     context 'when password is too long' do
+      let(:password) { ('m' * 73).to_s }
+      let(:user) { build(:user, password: password) }
       it 'validates password length', :aggregate_failures do
-        password = ('m' * 73).to_s
-        user = build(:user, password: password)
         expect(user).not_to be_valid
-        expect(user.errors).to match_array(['Password Password is too long'])
+        expect(user.errors).to match_array(['Password is too long'])
       end
     end
   end
