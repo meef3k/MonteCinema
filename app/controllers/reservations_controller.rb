efc85670c8f2defa53cdd6d12b6096d6ee273a38
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-09 19:53:29 +0200

    [feature] Add mailer to reservation create

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -18,6 +18,7 @@ class ReservationsController < ApplicationController
     rescue StandardError
       render :new, status: :unprocessable_entity and return
     end
+    ReservationMailer.with(reservation: @reservation).reservation_booked.deliver_later
     redirect_to seances_path, notice: 'Reservation was successfully created.'
   end
 
