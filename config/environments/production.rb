c3098b65b0724e05462aec6ea1a44a8708560365
Mateusz Jawor <mateuszjawor99@gmail.com>
2022-10-13 10:48:16 +0200

    Fix mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = {host: 'monte-cinema-meef1k.herokuapp.com', protocol: 'https'}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
